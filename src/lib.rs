52d27e42c25b0821b5599ad614f0b052519d6b3b
Mikolaj Wielgus <wielgusmikolaj@gmail.com>
2026-01-06 20:28:56 +0100

    Implement triangulation around inserted vertex

--- src/lib.rs
+++ src/lib.rs
@@ -168,7 +168,7 @@ impl<
     ) {
         let new_face = self.add_unwired_face(face_weight);
         let vertexes = self.add_unwired_polygon_vertexes(vertex_weights);
-        let edges = self.add_unwired_polygon_edges(edge_weights, &vertexes, new_face, outer_face);
+        let edges = self.add_unwired_polygon_edges(&vertexes, new_face, outer_face, edge_weights);
 
         self.wire_face_edges_vertexes(new_face, &edges);
     }
@@ -187,10 +187,10 @@ impl<
 
     fn add_unwired_polygon_edges(
         &mut self,
-        edge_weights: impl IntoIterator<Item = (HEW, HEW)>,
-        vertexes: &Vec<VertexId>,
+        vertexes: &[VertexId],
         new_face: FaceId,
         target_face: FaceId,
+        edge_weights: impl IntoIterator<Item = (HEW, HEW)>,
     ) -> Vec<EdgeId> {
         let vertexes_circular_pair_windows = vertexes
             .iter()
@@ -214,6 +214,122 @@ impl<
         }
 
         edges
+    }
+}
+
+impl<
+    VW: Clone,
+    HEW: Clone,
+    FW: Clone + maplike::Get<usize>,
+    VC: maplike::Get<usize, Item = Vertex<VW>> + maplike::Insert<usize> + maplike::Push<usize>,
+    HEC: maplike::Get<usize, Item = HalfEdge<HEW>> + maplike::Insert<usize> + maplike::Push<usize>,
+    FC: maplike::Get<usize, Item = Face<FW>> + maplike::Insert<usize> + maplike::Push<usize>,
+> Dcel<VW, HEW, FW, VC, HEC, FC>
+{
+    /// Partition a face into triangles by inserting a vertex inside and then
+    /// adding edges between it and the original face's vertexes.
+    ///
+    /// The original face is reused for the first triangle. New faces are
+    /// created for all the other triangles.
+    ///
+    /// Returns the new vertex id together with the face ids of all the new
+    /// triangles.
+    pub fn triangulate_around_vertex_with_all_weights(
+        &mut self,
+        perimeter_face: FaceId,
+        inner_vertex_weight: VW,
+        inner_edge_weights: impl IntoIterator<Item = (HEW, HEW)>,
+        triangle_face_weights: impl IntoIterator<Item = FW>,
+    ) {
+        let inner_vertex = self.add_unwired_vertex(inner_vertex_weight);
+        let triangle_faces =
+            self.add_unwired_triangulation_faces(perimeter_face, triangle_face_weights);
+        let inner_edges = self.add_unwired_triangulation_edges(
+            perimeter_face,
+            inner_vertex,
+            &triangle_faces,
+            inner_edge_weights,
+        );
+        self.wire_triangulation_faces_edges_vertexes(perimeter_face, &inner_edges);
+    }
+
+    fn add_unwired_triangulation_faces(
+        &mut self,
+        first_face: FaceId,
+        triangle_face_weights: impl IntoIterator<Item = FW>,
+    ) -> Vec<FaceId> {
+        let mut triangle_faces = vec![];
+
+        let mut face_weights_iter = triangle_face_weights.into_iter();
+        self.faces.insert(
+            first_face.0,
+            Face {
+                incident_half_edge: self.faces.get(&first_face.0).unwrap().incident_half_edge,
+                weight: face_weights_iter.next().unwrap(),
+            },
+        );
+        triangle_faces.push(first_face);
+
+        for face_weight in face_weights_iter {
+            triangle_faces.push(self.add_unwired_face(face_weight));
+        }
+
+        triangle_faces
+    }
+
+    fn add_unwired_triangulation_edges(
+        &mut self,
+        perimeter_face: FaceId,
+        inner_vertex: VertexId,
+        triangle_faces: &[FaceId],
+        inner_edge_weights: impl IntoIterator<Item = (HEW, HEW)>,
+    ) -> Vec<EdgeId> {
+        let mut inner_edge_weights = inner_edge_weights.into_iter();
+        let mut face_vertexes_walker = self.face_vertexes(perimeter_face);
+        let mut edges = vec![];
+
+        let mut i: usize = 0;
+
+        while let Some(perimeter_vertex) = face_vertexes_walker.next(self) {
+            let (weight, twin_weight) = inner_edge_weights.next().unwrap();
+
+            edges.push(self.add_unwired_edge(
+                perimeter_vertex,
+                inner_vertex,
+                triangle_faces[i.wrapping_sub_signed(1)],
+                triangle_faces[i],
+                weight,
+                twin_weight,
+            ));
+
+            i += 1;
+        }
+
+        edges
+    }
+
+    fn wire_triangulation_faces_edges_vertexes(
+        &mut self,
+        perimeter_face: FaceId,
+        inner_edges: &[EdgeId],
+    ) {
+        let inner_edges_circular_pair_windows = inner_edges
+            .iter()
+            .zip(inner_edges.iter().skip(1).chain(inner_edges.iter().take(1)));
+        let mut perimeter_half_edges_walker = self.face_half_edges(perimeter_face);
+
+        for (inner_edge, next_inner_edge) in inner_edges_circular_pair_windows {
+            let perimeter_half_edge = perimeter_half_edges_walker.next(self).unwrap();
+
+            self.wire_face_edges_vertexes(
+                perimeter_face,
+                &[
+                    self.full_edge(perimeter_half_edge),
+                    self.full_edge(next_inner_edge.0),
+                    self.full_edge(inner_edge.1),
+                ],
+            );
+        }
     }
 }
 
