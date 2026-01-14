03db46fda2340f5811f85a1c4f0fb1e66ab86878
Mikolaj Wielgus <wielgusmikolaj@gmail.com>
2026-01-14 17:41:12 +0100

    Make iterating over half-edges of unbounded face not panic too

--- src/iter.rs
+++ src/iter.rs
@@ -139,6 +139,20 @@ impl<'a, VW, HEW, FW, VC, HEC: Get<usize, Item = HalfEdge<HEW>>, FC> Iterator
 impl<VW, HEW, FW, VC, HEC, FC: Get<usize, Item = Face<FW>>> Dcel<VW, HEW, FW, VC, HEC, FC> {
     #[inline]
     pub fn face_half_edges(&self, face: FaceId) -> FaceHalfEdgesIter<'_, VW, HEW, FW, VC, HEC, FC> {
+        // Unbounded face has no half-edges. Since the unbounded face is
+        // supposed to behave similarly to other faces, it is better to branch
+        // out here than to have the code below panic.
+        if face == self.unbounded_face() {
+            return FaceHalfEdgesWalker {
+                // Uninitialized half-edge.
+                initial_half_edge: HalfEdgeId::new(0),
+                // Setting `curr_edge` to None makes the iterator produce no
+                // elements.
+                curr_half_edge: None,
+            }
+            .iter(self);
+        }
+
         let initial_half_edge = self
             .faces
             .get(&face.id())
@@ -193,10 +207,9 @@ impl<VW, HEW, FW, VC, HEC: Get<usize, Item = HalfEdge<HEW>>, FC: Get<usize, Item
 {
     #[inline]
     pub fn face_edges(&self, face: FaceId) -> FaceEdgesIter<'_, VW, HEW, FW, VC, HEC, FC> {
-        // Unbounded face has no edges.
-        // Since the unbounded face is supposed to behave similarly to other
-        // edges, it is better to branch out here than to have the code below
-        // panic.
+        // Unbounded face has no edges. Since the unbounded face is supposed to
+        // behave similarly to other faces, it is better to branch out here than
+        // to have the code below panic.
         if face == self.unbounded_face() {
             return FaceEdgesWalker {
                 // Uninitialized edge.
