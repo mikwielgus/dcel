be7012706c7dc5940d4e6c0abdadd948a881f37e
Mikolaj Wielgus <wielgusmikolaj@gmail.com>
2026-01-17 12:00:25 +0100

    Make repository REUSE-compliant

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
+<!--
+SPDX-FileCopyrightText: 2026 dcel contributors
 
+SPDX-License-Identifier: MIT OR Apache-2.0
+-->
--- src/walkers.rs
+++ src/walkers.rs
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2026 dcel contributors
+//
+// SPDX-License-Identifier: MIT OR Apache-2.0
+
 use maplike::Get;
 
 use crate::{Dcel, EdgeId, FaceId, HalfEdge, HalfEdgeId, Vertex, VertexId};
