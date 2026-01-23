5de2f9119c13f3688adf837042d55631e2fe174f
Mikolaj Wielgus <wielgusmikolaj@gmail.com>
2026-01-23 22:51:24 +0100

    Add tests for merging and absorbing faces over singular edges

--- src/merge.rs
+++ src/merge.rs
@@ -213,4 +213,47 @@ mod test {
         assert_face_boundary!(&dcel, 8, 6);
         assert_face_boundary!(&dcel, 9, 6);
     }
+
+    #[test]
+    fn test_merge_faces_over_edge() {
+        let mut dcel = init_dcel_with_3x3_hex_mesh!(StableDcel<(i32, i32)>);
+        dcel.merge_faces_over_edges_and_vertexes(
+            [FaceId::new(5), FaceId::new(6)],
+            [EdgeId::new(HalfEdgeId::new(44), HalfEdgeId::new(45))],
+            [],
+        );
+
+        assert_face_boundary!(&dcel, 0, 0);
+        assert_face_boundary!(&dcel, 1, 6);
+        assert_face_boundary!(&dcel, 2, 6);
+        assert_face_boundary!(&dcel, 3, 6);
+        assert_face_boundary!(&dcel, 4, 6);
+        assert_face_boundary!(&dcel, 5, 10);
+        // Face 6 does not exist.
+        assert_face_boundary!(&dcel, 7, 6);
+        assert_face_boundary!(&dcel, 8, 6);
+        assert_face_boundary!(&dcel, 9, 6);
+    }
+
+    #[test]
+    fn test_absorb_faces_over_edge() {
+        let mut dcel = init_dcel_with_3x3_hex_mesh!(StableDcel<(i32, i32)>);
+        dcel.absorb_faces_over_edges_and_vertexes(
+            FaceId::new(6),
+            [FaceId::new(5)],
+            [EdgeId::new(HalfEdgeId::new(44), HalfEdgeId::new(45))],
+            [],
+        );
+
+        assert_face_boundary!(&dcel, 0, 0);
+        assert_face_boundary!(&dcel, 1, 6);
+        assert_face_boundary!(&dcel, 2, 6);
+        assert_face_boundary!(&dcel, 3, 6);
+        assert_face_boundary!(&dcel, 4, 6);
+        // Face 5 does not exist.
+        assert_face_boundary!(&dcel, 6, 10);
+        assert_face_boundary!(&dcel, 7, 6);
+        assert_face_boundary!(&dcel, 8, 6);
+        assert_face_boundary!(&dcel, 9, 6);
+    }
 }
